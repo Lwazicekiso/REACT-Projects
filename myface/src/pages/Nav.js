039fac85079c3921304c30b5cdd43ed6ca9873e6
Lwazicekiso <132575366+Lwazicekiso@users.noreply.github.com>
2024-05-12 05:30:13 +0000

    removed clear button

--- myface/src/pages/Nav.js
+++ myface/src/pages/Nav.js
@@ -2,37 +2,40 @@ import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import Header from "./Header";
 
-const Nav = ({ searchVal, setSearchVal, searchFil, resetSearch }) => { // Receive props
-
+const Nav = ({ searchVal, setSearchVal }) => {
   return (
     <section>
-      <Header title='My Js Blog'/>
-      <nav> 
+      <Header title="My Js Blog" />
+      <nav>
         <ul>
           <li>
-            <form style={{ marginRight: '2%', }}> 
-              <input type='button' value={searchVal} placeholder="Search" onChange={e => setSearchVal(e.target.value)}/>
-              <button id='searchB' type='button' onClick={(e) => {searchFil();e.preventDefault()}}>Search</button>
-              <button onClick={(e)=>{e.preventDefault();resetSearch()}}>Clear Search</button>  
-            </form> 
-            </li>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/post">Post</Link>
-              </li>
-              <li>    
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li>
-              <Link to='/About'>About</Link>
-              </li>
-            </ul>
+            <form style={{ marginRight: "2%" }}>
+              <input
+                type="text"
+                value={searchVal}
+                placeholder="Search"
+                onChange={(e) => setSearchVal(e.target.value)}
+              />
+            </form>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/post">Post</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+        </ul>
       </nav>
       <Outlet />
     </section>
-  )
+  );
 };
 
 export default Nav;
+
